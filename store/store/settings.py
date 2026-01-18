5c812dc3354fa3f772fba7ba71d11d451d4482dc
jpazcoitia-sudo <jp.azcoitia@gmail.com>
2026-01-18 12:00:16 -0300

    Fix static assets path case (posAPP -> posApp) and static settings

--- store/store/settings.py
+++ store/store/settings.py
@@ -160,11 +160,8 @@ USE_TZ = False
 
 
 # original
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    # location of your application, should not be public web accessible 
-    './static',
-)
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
